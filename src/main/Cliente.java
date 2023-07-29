235549fe12fa603ae18f0eb757d2491e0226239b
AndreiBack <andreigregoriback@gmail.com>
2023-07-29 18:55:25 -0300

    Reorganização do codigo para ficar mais limpo

--- src/main/Cliente.java
+++ src/main/Cliente.java
@@ -1,16 +1,16 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Cliente {
     private int id;
-
     private String nome;
     private int idade;
-
     private List<Endereco> enderecos;
 
-    public Cliente(){
+    public Cliente() {
     }
 
     public Cliente(int id, String nome, int idade, List<Endereco> enderecos) {
@@ -50,5 +50,100 @@ public class Cliente {
 
     public void setEnderecos(List<Endereco> enderecos) {
         this.enderecos = enderecos;
+    }
+
+
+    public static void cadastrarCliente(List<Cliente> clientes, Scanner scan) {
+        System.out.println("Digite o nome: ");
+        scan.nextLine();
+        String nome = scan.nextLine();
+        System.out.println("Digite a idade: ");
+        int idade = scan.nextInt();
+
+        List<Endereco> enderecos = new ArrayList<>();
+        int cont = 1;
+        while (true) {
+            System.out.println("Endereço " + cont + " : (Digite 0 para sair)");
+
+            System.out.println("Digite o nome da rua: ");
+            scan.nextLine();
+            String rua = scan.nextLine();
+
+            if (rua.equals("0")) {
+                break;
+            }
+
+            System.out.println("Digite o numero da residência: ");
+            int numero = scan.nextInt();
+            enderecos.add(new Endereco(rua, numero));
+
+            cont++;
+        }
+
+        int id = clientes.size() + 1;
+
+        clientes.add(new Cliente(id, nome, idade, enderecos));
+    }
+
+    public static void editarCliente(List<Cliente> clientes, Scanner scan) {
+        Cliente cliente = buscarCliente(clientes, scan);
+
+        if (cliente == null) {
+            System.out.println("Cliente não encontrado!");
+            System.out.println("");
+            return;
+        }
+
+        mostrarCliente(cliente);
+
+        System.out.println("Editar: 1 - nome, 2 - idade, 3 - endereços");
+        int opcao = scan.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Digite o novo nome do cliente: ");
+                scan.nextLine();
+                String nome = scan.nextLine();
+                clientes.get(cliente.getId() - 1).setNome(nome);
+                System.out.println("Nome editado com sucesso!");
+                break;
+            case 2:
+                System.out.println("Digite a nova idade do cliente: ");
+                int idade = scan.nextInt();
+                clientes.get(cliente.getId() - 1).setIdade(idade);
+                System.out.println("Idade editada com sucesso!");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+    }
+
+    public static Cliente buscarCliente(List<Cliente> clientes, Scanner scan) {
+        System.out.println("Digite o nome do cliente: ");
+        scan.nextLine();
+        String nome = scan.nextLine();
+
+        for (Cliente cliente : clientes) {
+            if (cliente.getNome().equals(nome)) {
+                return cliente;
+            }
+        }
+
+        return null;
+    }
+
+    public static void mostrarClientes(List<Cliente> clientes) {
+        for (Cliente cliente : clientes) {
+            mostrarCliente(cliente);
+        }
+    }
+
+    public static void mostrarCliente(Cliente cliente) {
+        System.out.println(cliente.getNome() + ", " + cliente.getIdade() + " anos");
+        System.out.println("Endereços:");
+        for (int i = 0; i < cliente.getEnderecos().size(); i++) {
+            System.out.println((i + 1) + " - " + cliente.getEnderecos().get(i).getRua() + ", " + cliente.getEnderecos().get(i).getNumero());
+        }
+        System.out.println("");
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,25 +10,39 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         while (true) {
-            switch (menu(scan)){
-                case 1 -> mostrarClientes(clientes);
+            switch (menu(scan)) {
+                case 1:
+                    Cliente.mostrarClientes(clientes);
+                    break;
 
-                case 2 -> cadastrarCliente(clientes, scan);
+                case 2:
+                    Cliente.cadastrarCliente(clientes, scan);
+                    break;
 
-                case 3 -> editarCliente(clientes, scan);
+                case 3:
+                    Cliente.editarCliente(clientes, scan);
+                    break;
 
-                case 4 -> cadastrarCliente(clientes, scan);
+                case 4:
 
-                case 5 -> editarCliente(clientes, scan);
+                    break;
 
-                case 6 -> System.exit(0);
+                case 5:
 
-                default -> System.out.println("Valor inválido!");
+                    break;
+
+                case 6:
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Valor inválido!");
+                    break;
             }
         }
     }
 
-    static int menu(Scanner scan){
+    static int menu(Scanner scan) {
         int pedidosAbertos;
         int pedidosEncerrados;
 
@@ -43,98 +57,5 @@ public class Main {
         System.out.println("Pedidos no dia: " + " Pedidos abertos: " + " Pedidos encerrados: ");
 
         return scan.nextInt();
-    }
-
-    static void cadastrarCliente(List<Cliente> clientes, Scanner scan){
-        System.out.println("Digite o nome: ");
-        String nome = scan.next();
-        System.out.println("Digite a idade: ");
-        int idade = scan.nextInt();
-
-        List<Endereco> enderecos = new ArrayList<>();
-        int cont = 1;
-        while (true) {
-            System.out.println("Endereço " + cont + " : (Digite 0 para sair)");
-
-            System.out.println("Digite o nome da rua: ");
-            String rua = scan.next();
-
-            if (rua.equals("0")) {
-                break;
-            }
-
-            System.out.println("Digite o numero da residência: ");
-            int numero = scan.nextInt();
-            enderecos.add(new Endereco(rua, numero));
-
-            cont++;
-        }
-
-        int id = clientes.size() + 1;
-
-        clientes.add(new Cliente(id, nome, idade, enderecos));
-    }
-
-    static void editarCliente(List<Cliente> clientes, Scanner scan){
-        Cliente cliente = buscarCliente(clientes, scan);
-
-        if (cliente == null){
-            System.out.println("Cliente não encontrado!");
-            System.out.println("");
-            return;
-        }
-
-        mostrarCliente(cliente);
-
-        System.out.println("Editar: 1 - nome, 2 - idade, 3 - endereços");
-        int opcao = scan.nextInt();
-
-        switch (opcao){
-            case 1 -> {
-                System.out.println("Digite o novo nome do cliente: ");
-                String nome = scan.next();
-                clientes.get(cliente.getId() - 1).setNome(nome);
-                System.out.println("Nome editado com sucesso!");
-            }
-            case 2 -> {
-                System.out.println("Digite a nova idade do cliente: ");
-                int idade = scan.nextInt();
-                clientes.get(cliente.getId() - 1).setIdade(idade);
-                System.out.println("Idade editada com sucesso!");
-            }
-            default -> {
-                System.out.println("Opção inválida!");
-            }
-        }
-    }
-
-    static Cliente buscarCliente(List<Cliente> clientes, Scanner scan){
-        System.out.println("Digite o nome do cliente: ");
-        String nome = scan.next();
-
-        for (Cliente cliente : clientes)
-        {
-            if (cliente.getNome().equals(nome)){
-                return cliente;
-            }
-        }
-
-        return null;
-    }
-
-    static void mostrarClientes(List<Cliente> clientes){
-        for(Cliente cliente : clientes){
-            mostrarCliente(cliente);
-        }
-    }
-
-    static void mostrarCliente(Cliente cliente){
-        System.out.println(cliente.getNome() + ", " + cliente.getIdade() + " anos");
-        System.out.println("Endereços:");
-        for (int i = 0; i < cliente.getEnderecos().size(); i++)
-        {
-            System.out.println((i+1) + " - " + cliente.getEnderecos().get(i).getRua() + ", " + cliente.getEnderecos().get(i).getNumero());
-        }
-        System.out.println("");
     }
 }
